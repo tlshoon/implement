25700d83c064de282fbaa5cb52cc5563a58fa276
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-09-08 12:15:19 +0900

    Add files via upload

--- 구현.py
+++ 구현.py
@@ -19,7 +19,7 @@ for plan in plans:
 print(x,y)'''
 
 # 시각
-h = int(input())
+'''h = int(input())
 
 count = 0
 
@@ -30,5 +30,22 @@ for i in range(h+1):
             if '3' in str(i) + str(j) + str(k):
                 count += 1
 
-print(count)
+print(count)'''
+
+# 왕실의 나이트
+'''input_data = input()
+row = int(input_data[1])
+coloum = int(ord(input_data[0])) - int(ord('a')) + 1
+
+steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
+
+result = 0
+
+for step in steps:
+    next_row = row + step[0]
+    next_coloum = coloum + step[1]
+    if next_row >= 1 and next_row <=8 and next_coloum >= 1 and next_coloum <=8:
+        result += 1
+
+print(result)'''
 
