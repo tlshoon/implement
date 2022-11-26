a04a112e1458ec2ec6fd253386a9e0b9a1caffbe
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-11-26 17:37:31 +0900

    Add files via upload

--- 구현.py
+++ 구현.py
@@ -1,54 +1,56 @@
 # 상하좌우
-'''n = int(input())
-x, y = 1, 1
-plans = input().split()
-
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
-move_tpyes = ['L','R','U','D']
-
-for plan in plans:
-    for i in range(len(move_tpyes)):
-        if plan == move_tpyes[i]:
-            nx = x + dx[i]
-            ny = x + dy[i]
-    if nx < 1 or ny <1 or nx > n or ny > n:
-        continue
-    x, y = nx, ny
-
-print(x,y)'''
+# n = int(input())
+# x, y = 1, 1
+# plans = input().split()
+#
+# dx = [0,0,-1,1]
+# dy = [-1,1,0,0]
+# move_tpyes = ['L','R','U','D']
+#
+# for plan in plans:
+#     for i in range(len(move_tpyes)):
+#         if plan == move_tpyes[i]:
+#             nx = x + dx[i]
+#             ny = x + dy[i]
+#     if nx < 1 or ny <1 or nx > n or ny > n:
+#         continue
+#     x, y = nx, ny
+#
+# print(x,y)
 
 # 시각
-'''h = int(input())
-
-count = 0
-
-for i in range(h+1):
-    for j in range(60):
-        for k in range(60):
-
-            if '3' in str(i) + str(j) + str(k):
-                count += 1
-
-print(count)'''
+# h = int(input())
+#
+# count = 0
+#
+# for i in range(h+1):
+#     for j in range(60):
+#         for k in range(60):
+#
+#             if '3' in str(i) + str(j) + str(k):
+#                 count += 1
+#
+# print(count)
 
 # 왕실의 나이트
-'''input_data = input()
-row = int(input_data[1])
-coloum = int(ord(input_data[0])) - int(ord('a')) + 1
+# input_data = input()
+# row = int(input_data[1])
+# coloum = int(ord(input_data[0])) - int(ord('a')) + 1
+#
+# steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
+#
+# result = 0
+#
+# for step in steps:
+#     next_row = row + step[0]
+#     next_coloum = coloum + step[1]
+#     if next_row >= 1 and next_row <=8 and next_coloum >= 1 and next_coloum <=8:
+#         result += 1
+#
+# print(result)
 
-steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
 
-result = 0
-
-for step in steps:
-    next_row = row + step[0]
-    next_coloum = coloum + step[1]
-    if next_row >= 1 and next_row <=8 and next_coloum >= 1 and next_coloum <=8:
-        result += 1
-
-print(result)'''
-
+# 게임 개발
 # n,m = map(int, input().split())
 #
 # check =[[0]*m for _ in range(n)] # 방문할 위치를 저장할 배열 선언
@@ -97,6 +99,9 @@ print(result)'''
 #                 break
 # print(count)
 
+######################## 3장 문제 ########################
+
+
 # 럭키 스트레이트
 # n = input
 # length = len(n)
@@ -130,3 +135,4 @@ print(result)'''
 #     result.append(str(value))
 #
 # print(''.join(result))  # 요소 하나하나 합쳐서 반환
+
